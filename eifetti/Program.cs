ff394a9dede7ff611d76dd30cfb7ccf5f923f3e8
dejan <dejan@DESKTOP-HA22HU0>
2023-02-28 18:14:58 +0100

    Projektdateien hinzufügen.

---
+++ eifetti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Threading;
+using System.Xml.Linq;
+
+Console.WriteLine(aufgabe());
+static string aufgabe()
+{
+    Console.Write($"Gebe deinen Namen ein: ");
+    string name=Console.ReadLine();
+    Console.Write($"Be nenne das Fach: ");
+    string fach = Console.ReadLine();
+    Console.Write($"Gebe die Note ein: ");
+    string note = Console.ReadLine();
+    Console.Write($"Gib das Alter an: ");
+    string alter = Console.ReadLine();
+
+    string text1 = "Hallo A21!!!! Guten Tag Herr ";
+    string text2 = " Sie studieren ";
+    string text3 = " haben die Note ";
+    string text4 = " und sind ";
+    string text5 = " Jahre alt ";
+    return text1+name+text2+fach+text3+note+text4+alter+text5;
+}
+
